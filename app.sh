5553340bb4a1a53160d3b582f4b8d432b020ca27
yskszk63 <yskszk63@gmail.com>
2023-02-12 12:16:00 +0900

    add `PROXY_TO` and no ephemeral node.

--- app.sh
+++ app.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-[ -n "$TS_API_CLIENT_ID" -a -n "$TS_API_CLIENT_SECRET" ] || {
+[[ -n "$PROXY_TO" ]] || {
+    echo '`PROXY_TO` not specified. Exit.' >&2
+    exit 1
+}
+
+( [[ -n "$TS_API_CLIENT_ID" ]] && [[ -n "$TS_API_CLIENT_SECRET" ]] ) || {
     echo '`TS_API_CLIENT_ID` or `TS_API_CLIENT_SECRET` not specified. Exit.' >&2
     exit 1
 }
 
 export TS_STATE_DIR=/var/lib/tailscale
 export TS_SOCKET=/var/run/tailscale/tailscaled.sock
-export TS_AUTH_KEY="$(get-authkey -tags "$AUTHKEY_TAG" -ephemeral)"
+export TS_AUTH_KEY="$(get-authkey -tags "$AUTHKEY_TAG")"
 if [[ -n "$TS_HOSTNAME" ]]; then
     export TS_EXTRA_ARGS="$TS_EXTRA_ARGS --hostname $TS_HOSTNAME"
     unset TS_HOSTNAME
@@ -15,7 +20,7 @@ fi
 containerboot &
 
 retry=0
-retry_max=20
+retry_max=100
 sleep 0.1
 until tailscale --socket $TS_SOCKET status > /dev/null || [[ retry -eq $retry_max ]]; do
     echo '##' $retry / $retry_max
@@ -29,6 +34,6 @@ dnsname=$(tailscale --socket $TS_SOCKET status --json |jq .Self.DNSName -r|sed -
     echo 'Failed to get DNSName.' >&2
     exit 1
 }
-caddy reverse-proxy --from "$dnsname" --to example.com --change-host-header &
+caddy reverse-proxy --from "$dnsname" --to "$PROXY_TO" --change-host-header &
 
 wait
